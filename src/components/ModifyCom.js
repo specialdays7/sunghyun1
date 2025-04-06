aaa941e5900c16cbee2a919667bdb3ed2eb1f5be
sunghyun <specialdays227@gamil.com>
2025-04-06 11:17:55 +0900

    ModifyCom 수정 추가

--- src/components/ModifyCom.js
+++ src/components/ModifyCom.js
@@ -9,8 +9,19 @@ function ModifyCom({ id, name, addr, onChange, onSubmit, loading, error }) {
         <form onSubmit={onSubmit}>
           <h3>회원 정보 수정</h3>
           <p>아이디: {id}</p>
-          <input type="text" name="name" value={name || ""} onChange={onChange} placeholder="이름" /><br />
-          <input type="text" name="addr" value={addr || ""} onChange={onChange} placeholder="주소" /><br />
+
+          <label>
+            이름: &nbsp;
+            <input type="text" name="name" value={name || ""} onChange={onChange} placeholder="이름을 입력하세요" />
+          </label>
+          <br /><br />
+
+          <label>
+            주소: &nbsp;
+            <input type="text" name="addr" value={addr || ""} onChange={onChange} placeholder="주소를 입력하세요" />
+          </label>
+          <br /><br />
+
           <button>수정 완료</button>
         </form>
       )}
